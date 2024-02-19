a70a8215434c2d80f540f52512ba4853e3c91a89
DmitriySmolin <45199267+DmitriySmolin@users.noreply.github.com>
2024-02-19 10:53:36 +0000

    add 2. Маршрутизация(Route)

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/workers', function () {
+  dd('dfgdfgdfg'); //die dump
+  return 'this workers is from routes';
+});
