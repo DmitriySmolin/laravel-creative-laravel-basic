76c96d03caabcbda5f7e31b9f38e5d52ce80ec36
Dmitriy Smolin <45199267+DmitriySmolin@users.noreply.github.com>
2024-03-21 11:55:04 +0000

    add 17. Консольная команда

---
+++ app/Console/Commands/DevCommand.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class DevCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'develop';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Some develops';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        dd(111111);
+    }
+}
